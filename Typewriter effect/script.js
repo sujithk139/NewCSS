34bd7f5a9a83647471c6b53bef948710f3a18540
sujith k <ksujith139@gmail.com>
2022-06-30 14:25:58 +0530

    typewriter effect added

---
+++ Typewriter effect/script.js
@@ -0,0 +1,5 @@
+const typeWriter = document.getElementById('typewriter-text');
+const text = 'Lorejileg iwjgwle gjslgs.';
+
+typeWriter.innerHTML = text;
+typeWriter.style.setProperty('--characters',text.length)
